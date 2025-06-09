bebdd32858e9bdb14240166f2212b2c56dce324d
Damien Ferbach <ferbach.damien@gmail.com>
2025-06-09 00:39:29 -0400

    sbatch args sweep.sh

--- dana-nonquadratic-tests/gpt2/Sweep.sh
+++ dana-nonquadratic-tests/gpt2/Sweep.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
+#SBATCH --output=gpt2_sweep_.out
+#SBATCH --error=gpt2_sweep.err
+#SBATCH --time=48:00:00
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:80GB
+#SBATCH --partition=unkillable
 
 for p in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
 do
     echo "Running with g3p = $p"
-    python nanogpt_log_losses_dana.py --train_steps=100000 --batch_size=32 --val_batch_size=32 --seq_len=1024 --dana_g2=0.05 --dana_g3_iv=0.01 --dana_g3_p=-$p
+    python nanogpt_log_losses_dana.py --train_steps=50000 --batch_size=32 --val_batch_size=32 --seq_len=1024 --dana_g2=0.05 --dana_g3_iv=0.01 --dana_g3_p=-$p
 done
