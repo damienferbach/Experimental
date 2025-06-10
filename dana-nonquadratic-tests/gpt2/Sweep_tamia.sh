73fe6f24dcdabacf98dceb2d3bd429f406248729
Damien Ferbach <ferbach.damien@gmail.com>
2025-06-10 15:24:38 -0400

    tamia configs

--- dana-nonquadratic-tests/gpt2/Sweep_tamia.sh
+++ dana-nonquadratic-tests/gpt2/Sweep_tamia.sh
@@ -15,5 +15,5 @@ source ~/links/projects/aip-gidelgau/dferbach/my-envs/dana-env/bin/activate
 for p in 1.0
 do
     echo "Running with g3p = $p"
-    python nanogpt_log_losses_dana.py --train_steps=500 --batch_size=32 --val_batch_size=32 --seq_len=32 --dana_g2=0.05 --dana_g3_iv=0.01 --dana_g3_p=-$p --weight_decay=0.01 #--wandb="yes" --path=""
+    python nanogpt_log_losses_dana.py --train_steps=100 --batch_size=32 --val_batch_size=32 --seq_len=32 --dana_g2=0.05 --dana_g3_iv=0.01 --dana_g3_p=-$p --weight_decay=0.01
 done
