a0f652f55fc920e7fed8c54ce156b556652aa9f6
Damien Ferbach <ferbach.damien@gmail.com>
2025-06-12 03:09:56 -0400

    sweep

--- dana-nonquadratic-tests/gpt2/Sweep_tamia_parallel.sh
+++ dana-nonquadratic-tests/gpt2/Sweep_tamia_parallel.sh
@@ -5,7 +5,7 @@
 #SBATCH --gpus-per-node=h100:4
 #SBATCH --cpus-per-gpu=8
 #SBATCH --mem=0                    # “alloc as needed” on Alliance
-#SBATCH --job-name=hpo-array
+#SBATCH --job-name=sweep_g2
 
 export CLUSTER=tamia
 export WANDB_API_KEY=bece9f2099e3e85e0ae9922002616cf20bd26946
@@ -19,32 +19,31 @@ echo "activated env"
 
 # MANUAL SWEEP
 # Launch four copies in parallel; each sees one GPU
-# srun --ntasks=4 --cpus-per-task=$SLURM_CPUS_PER_GPU \
-#      --gpus-per-task=h100:1 --gpu-bind=single:1 --output=logs/%x-%j_%t.out --error=logs/%x-%j_%t.err --exclusive \
-#      bash -c '
-#         i=$SLURM_LOCALID                 # 0..3
-#         case $i in
-#           0) wd=1.0 ;;
-#           1) wd=0.1 ;;
-#           2) wd=0.01 ;;
-#           3) wd=0.001 ;;
-#         esac
-#         python nanogpt_log_losses_dana.py --train_steps=100000 --batch_size=32 --val_batch_size=32 --seq_len=1024 --dana_g2=0.05 --dana_g3_iv=0.01 --dana_g3_p=-5.0 --weight_decay=${wd}
-#      '
+srun --ntasks=4 --cpus-per-task=$SLURM_CPUS_PER_GPU \
+     --gpus-per-task=h100:1 --gpu-bind=single:1 --output=logs/%x-%j_%t.out --error=logs/%x-%j_%t.err --exclusive \
+     bash -c '
+        i=$SLURM_LOCALID                 # 0..3
+        case $i in
+          0) dana_g2=4.0 ;;
+          1) dana_g2=1.0 ;;
+          2) dana_g2=0.25 ;;
+          3) dana_g2=0.0625 ;;
+        esac
+        python nanogpt_log_losses_dana.py --train_steps=100000 --batch_size=32 --val_batch_size=32 --seq_len=1024 --dana_g2=${dana_g2} --dana_g3_iv=0.2 --dana_g3_p=-5.0 --weight_decay=0.00
+     '
 
 # WANDB SWEEP
 # Launch sweep and capture the Sweep ID
-SWEEP_ID=$(wandb sweep sweep_config.yaml 2>&1| grep -o 'wandb agent .*' | awk  '{print $NF}')
-echo "Created sweep with ID: $SWEEP_ID"
+# SWEEP_ID=$(wandb sweep sweep_config.yaml | grep "Created sweep with ID:" | awk '{print $NF}')
+# echo "Created sweep with ID: $SWEEP_ID"
 
-# Check if sweep creation succeeded
-if [ -z "$SWEEP_ID" ]; then
-    echo $SWEEP_ID
-    echo "Failed to create sweep or extract Sweep ID!"
-    exit 1
-fi
+# # Check if sweep creation succeeded
+# if [ -z "$SWEEP_ID" ]; then
+#     echo "Failed to create sweep or extract Sweep ID!"
+#     exit 1
+# fi
 
-# Start 4 parallel agents (1 per GPU)
-srun --ntasks=4 --cpus-per-task=$SLURM_CPUS_PER_GPU \
-     --gpus-per-task=h100:1 --gpu-bind=single:1 --exclusive \
-     bash -c "export CUDA_VISIBLE_DEVICES=\$SLURM_LOCALID && wandb agent $SWEEP_ID"
+# # Start 4 parallel agents (1 per GPU)
+# srun --ntasks=4 --cpus-per-task=$SLURM_CPUS_PER_GPU \
+#      --gpus-per-task=1 --gpu-bind=single:1 --exclusive \
+#      bash -c "export CUDA_VISIBLE_DEVICES=\$SLURM_LOCALID && wandb agent $SWEEP_ID"
