73607906fe9af7995bf6d17917585aa588ce1adb
Damien Ferbach <ferbach.damien@gmail.com>
2025-06-21 14:23:30 -0400

    sh script rmsprop

--- dana-nonquadratic-tests/gpt2/Sweep_mila.sh
+++ dana-nonquadratic-tests/gpt2/Sweep_mila.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #SBATCH --output=gpt2_sweep.out
 #SBATCH --error=gpt2_sweep.err
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:80GB
 #SBATCH --partition=unkillable
@@ -13,5 +13,5 @@ conda activate dana-env
 # for p in 5.0
 # do
 #     echo "Running with g3p = $p"
-python nanogpt_log_losses_dana.py --train_steps=100000 --batch_size=32 --val_batch_size=32 --seq_len=256 --dana_g2=0.05 --dana_g3_iv=0.01 --dana_g3_p=-0.0 --weight_decay=0.0 --wandb=True --optimizer="rmsprop_dana" --learning_rate=0.01 --beta_2=0.999
+python nanogpt_log_losses_dana.py --train_steps=100000 --batch_size=32 --val_batch_size=32 --seq_len=1024 --dana_g2=0.05 --dana_g3_iv=0.01 --dana_g3_p=-0.0 --weight_decay=0.0 --wandb=True --optimizer="rmsprop" --learning_rate=0.0001 --beta_2=0.999
 # done
