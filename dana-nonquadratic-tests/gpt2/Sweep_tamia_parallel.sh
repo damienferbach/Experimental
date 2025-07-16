6e62579ae681a52e19422242c27e6ffccb1a243e
Damien Ferbach <dferbach@tamia3.tamia.ecpia.ca>
2025-07-16 05:31:57 -0400

    tamia parallel

--- dana-nonquadratic-tests/gpt2/Sweep_tamia_parallel.sh
+++ dana-nonquadratic-tests/gpt2/Sweep_tamia_parallel.sh
@@ -24,10 +24,10 @@ srun --ntasks=4 --cpus-per-task=$SLURM_CPUS_PER_GPU \
      bash -c '
         i=$SLURM_LOCALID                 # 0..3
         case $i in
-          0) dana_kappa=0.0 ;;
-          1) dana_kappa=0.2 ;;
-          2) dana_kappa=0.4 ;;
-          3) dana_kappa=0.6 ;;
+          0) dana_kappa=0.8 ;;
+          1) dana_kappa=0.9 ;;
+          2) dana_kappa=1.0 ;;
+          3) dana_kappa=5.0 ;;
         esac
         python ../../timescale-experiment/nanogpt_rmsprop_dana_baseline_mixed_bf16_rope.py --train_steps=100000 --batch_size=32 --val_batch_size=32 --seq_len=1024 --dana_g2=0.05 --dana_g3=0.01 --dana_kappa=${dana_kappa} --wandb=True --data_root="~/links/scratch/fineweb/sample/10BT"
   '
