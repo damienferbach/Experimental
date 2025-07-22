58ea05bc9e5c50eb81bd9c1d04a1fef8b18f3192
Damien Ferbach <ferbach.damien@gmail.com>
2025-07-22 13:22:44 -0400

    sh tamia

--- dana-nonquadratic-tests/gpt2/Sweep_tamia_parallel_multi_gpu.sh
+++ dana-nonquadratic-tests/gpt2/Sweep_tamia_parallel_multi_gpu.sh
@@ -8,7 +8,7 @@
 #SBATCH --job-name=multi_gpu_tamia
 #SBATCH --output=logs/%x-%j.out
 #SBATCH --error=logs/%x-%j.err
-
+ 
 export NCCL_DEBUG=INFO
 export CLUSTER=tamia
 export WANDB_API_KEY=bece9f2099e3e85e0ae9922002616cf20bd26946
