bfbb13471eb934137b66f87e539425620193ebbc
Damien Ferbach <ferbach.damien@gmail.com>
2025-07-22 13:18:17 -0400

    change env tamia sh

--- dana-nonquadratic-tests/gpt2/Sweep_tamia_parallel_multi_gpu.sh
+++ dana-nonquadratic-tests/gpt2/Sweep_tamia_parallel_multi_gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --account=aip-gidelgau
-#SBATCH --time=24:00:00
+#SBATCH --time=0:10:00
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=h100:4
 #SBATCH --cpus-per-gpu=8
@@ -11,14 +11,25 @@
 
 export CLUSTER=tamia
 export WANDB_API_KEY=bece9f2099e3e85e0ae9922002616cf20bd26946
-module load arrow/18.1.0
+
+# module load arrow/18.1.0
+# #module load python/3.10.13
+# module load python/3.11.5
+# module load cuda/12.6
+# module load httpproxy
+# echo "loaded modules"
+# source ~/links/projects/aip-gidelgau/dferbach/my-envs/dana-env/bin/activate
+# echo "activated env"
+
+
 #module load python/3.10.13
 module load python/3.11.5
 module load cuda/12.6
+module load arrow/19.0.1
 module load httpproxy
 echo "loaded modules"
-source ~/links/projects/aip-gidelgau/dferbach/my-envs/dana-env/bin/activate
+source ~/links/projects/aip-gidelgau/dferbach/my-envs/dana-env-multi-gpu/bin/activate
 echo "activated env"
 
 dana_g3_p=1.0
-python nanogpt_log_losses_dana_multi_gpu.py --train_steps=100000 --batch_size=32 --val_batch_size=32 --seq_len=1024 --dana_g2=0.25 --dana_g3_iv=0.2 --dana_g3_p=-${dana_g3_p} --weight_decay=0.0 --wandb=True --optimizer="dana" --model="GPT2-"
+python nanogpt_log_losses_dana_multi_gpu.py --train_steps=100000 --batch_size=32 --val_batch_size=32 --seq_len=1024 --dana_g2=0.005 --dana_g3_iv=0.2 --dana_g3_p=-${dana_g3_p} --weight_decay=0.0 --wandb=True --optimizer="dana" --model="GPT2-large"
